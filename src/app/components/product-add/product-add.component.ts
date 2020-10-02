2ead38adffa0f92ed59e50fa1bdeee6b911aed6f
puff1g <tke2000wow@gmail.com>
2020-10-02 12:54:42 +0200

    Changes

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -9,7 +9,6 @@ import { ProductService } from "src/app/services/product.service";
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
-
   product = {
     name: '',
     description: '',
@@ -22,34 +21,37 @@ export class ProductAddComponent implements OnInit {
   constructor(private ProductService: ProductService) { }
 
   ngOnInit() {
+      
   }
-
+  
   saveProduct() {
     const data = {
       name: this.product.name,
-      description: this.product.description
+      description: this.product.description,
+      pris: this.product.pris,
+      amount: this.product.amount,
     };
-
+    console.log(data),
     this.ProductService.create(data)
-      .subscribe(
-        response => {
-          console.log(response);
-          this.submitted = true;
-        },
-        error => {
-          console.log(error);
-        });
-  }
-
-  newProduct() {
-    this.submitted = false;
-    this.product = {
-      name: '',
-      description: '',
-      pris: '',
-      amount: '',
-      published: false
-    };
+    .subscribe(
+      response => {
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      });
+    }
+    
+    newProduct() {
+      this.submitted = false;
+      this.product = {
+        name: '',
+        description: '',
+        pris: '',
+        amount: '',
+        published: false
+      };
   }
 
 }
